b5b4bc5d19f4fe5de5f60929a91ed467368b09d4
Paul Page <pjpage98@gmail.com>
2023-11-13 11:12:47 -0600

    This is now my experiments repo, moved sokol stuff to a subfolder and added a rust raylib example

--- sokol/src/main.c
+++ sokol/src/main.c
@@ -106,11 +106,11 @@ void event(const sapp_event* e) {
             sapp_request_quit();
         }
     } else if (e->type == SAPP_EVENTTYPE_MOUSE_MOVE) {
-        /* state.pos_x = gl_x(e->mouse_x); */
-        /* state.pos_y = gl_y(e->mouse_y); */
+        state.pos_x = gl_x(e->mouse_x);
+        state.pos_y = gl_y(e->mouse_y);
     } else if (e->type == SAPP_EVENTTYPE_MOUSE_SCROLL) {
         /* state.pos_x = gl_x(e->scroll_x); */
-        state.pos_y -= e->scroll_y / state.window_height * 10.0;
+        /* state.pos_y -= e->scroll_y / state.window_height * 10.0; */
     }
 }
 
